df4ae353497a27c2b820d2a382a3bbc9e12ecab2
EladAM52 <eladamir46@gmail.com>
2024-02-21 23:34:20 +0200

    .

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,77 +1,94 @@
-import React, { useState } from 'react';
-import Swal from 'sweetalert2';
-import { useNavigate } from 'react-router-dom';
-import '../css/login.css';
+import React, { useState } from "react";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+import "../css/login.css";
 
 function Login() {
-    const navigate = useNavigate();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const handleLogin = async (e) => {
-        e.preventDefault(); // Prevent the default form submission
+  const handleLogin = async (e) => {
+    e.preventDefault(); // Prevent the default form submission
 
-        const response = await fetch('/login', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            body: JSON.stringify({ email, password }),
-        });
+    const response = await fetch("/login", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ email, password }),
+    });
 
-        const data = await response.json();
+    const data = await response.json();
 
-        if (data.success) {
-            localStorage.setItem('username',data.username)
-            Swal.fire({
-                icon: 'success',
-                title: 'התחברות מוצלחת',
-                text: 'תועבר בקרוב.',
-                showConfirmButton: false,
-                timer: 1500
-            }).then(() => {
-                setEmail('');
-                setPassword('');
-                if(data.role === 'admin'){
-                    navigate('/manager');
-                }
-                else{
-                    navigate('/employee');
-                }
-            });
+    if (data.success) {
+      localStorage.setItem("Username", data.username);
+      localStorage.setItem("UserRole", data.role);
+      Swal.fire({
+        icon: "success",
+        title: "התחברות מוצלחת",
+        text: "תועבר בקרוב.",
+        showConfirmButton: false,
+        timer: 1500,
+      }).then(() => {
+        setEmail("");
+        setPassword("");
+        if (data.role === "admin") {
+          navigate("/manager");
+        } else {
+          navigate("/employee");
         }
-        else {
-            Swal.fire({
-                icon: 'error',
-                title: 'התחברות נכשלה',
-                text: 'נסיון ההתחברות שלך לא הצליח. אנא נסה שנית.',
-                confirmButtonText: 'סגור'
-            }).then(() => {
-                setEmail('');
-                setPassword('');
-            });
-        }
-    };
+      });
+    } else {
+      Swal.fire({
+        icon: "error",
+        title: "התחברות נכשלה",
+        text: "נסיון ההתחברות שלך לא הצליח. אנא נסה שנית.",
+        confirmButtonText: "סגור",
+      }).then(() => {
+        setEmail("");
+        setPassword("");
+      });
+    }
+  };
 
-    return (
-        <div>
-            <div id="userinfo" style={{ position: 'absolute', right: '10px', top: '10px', display: 'none' }}>
-                <span id="uname"> </span> 
-                <button id="logout-button">התנתקות</button>
-            </div>
-            <form id="loginForm" onSubmit={handleLogin} dir="rtl">
-                <div className="text-center mb-4"> 
-                    <img src="../images/logo.png" alt="Logo" className="logo" /> 
-                </div>
-                <h3>כניסה למערכת</h3>
-                <span className="input-group-text" id="basic-addon1"><i className="fas fa-envelope"></i></span>
-                <input type="email" placeholder="דואר אלקטרוני" id="email" name="email" required value={email} onChange={(e) => setEmail(e.target.value)} />
-                <span className="input-group-text" id="basic-addon2"><i className="fas fa-lock"></i></span>
-                <input type="password" placeholder="סיסמה" id="password" name="password" required value={password} onChange={(e) => setPassword(e.target.value)} />
-                <button type="submit" id="loginButton">התחברות</button>
-            </form>
+  return (
+    <div>
+      <form id="loginForm" onSubmit={handleLogin} dir="rtl">
+        <div className="text-center mb-4">
+          <img src="../images/logo.png" alt="Logo" className="logo" />
         </div>
-    );
+        <h3>כניסה למערכת</h3>
+        <span className="input-group-text" id="basic-addon1">
+          <i className="fas fa-envelope"></i>
+        </span>
+        <input
+          type="email"
+          placeholder="דואר אלקטרוני"
+          id="email"
+          name="email"
+          required
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <span className="input-group-text" id="basic-addon2">
+          <i className="fas fa-lock"></i>
+        </span>
+        <input
+          type="password"
+          placeholder="סיסמה"
+          id="password"
+          name="password"
+          required
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <button type="submit" id="loginButton">
+          התחברות
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
--- client/src/components/manager.js
+++ client/src/components/manager.js
@@ -1,74 +1,56 @@
-import React, { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import '../css/manager.css'; // Adjust the path as necessary
-import Menu from './Menu';
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import "../css/manager.css";
+import Menu from "./Menu";
+import LogoutButton from "./LogoutButton";
+import WelcomeContainer from "./welcomeContainer";
 
 function Manager() {
   const navigate = useNavigate();
+  const [userRole, setUserRole] = useState("");
+  const [userName, setUserName] = useState("");
   const [authorized, setAuthorized] = useState(false);
-  const [activeMenuItem, setActiveMenuItem] = useState('');
-  const username=localStorage.getItem('username');
+  useEffect(() => {
+    const userRoleFromLocal = localStorage.getItem("UserRole");
+    setUserRole(userRoleFromLocal);
+  }, []);
 
-    const handleLogout = () => {
-        fetch('/logout', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-        })
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            if (data.success) {
-                window.location.href = '/login';
-            }
-        })
-        .catch(error => {
-            console.error('There has been a problem with your fetch operation:', error);
-        });
-    };
-    
-    useEffect(() => {
-      fetch('/manager')
-        .then(response => {
-          if (response.ok) {
-            return response.json();
-          } else {
-            throw new Error('Not authorized');
-          }
-        })
-        .then(data => {
-          if (data.success) {
-            setAuthorized(true);
-          } else {
-            navigate('/login');
-          }
-        })
-        .catch(error => {
-          console.error('Error:', error);
-          navigate('/login');
-        });
-    }, [navigate]);
-  
-    if (!authorized) {
-      return <div>Loading or not authorized...</div>;
-    }
+  useEffect(() => {
+    const userNameFromLocal = localStorage.getItem("Username");
+    setUserName(userNameFromLocal);
+  }, []);
 
+  useEffect(() => {
+    fetch("/manager")
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error("Not authorized");
+        }
+      })
+      .then((data) => {
+        if (data.success) {
+          setAuthorized(true);
+        } else {
+          navigate("/login");
+        }
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+        navigate("/login");
+      });
+  });
 
-    return (
-        <div>
-      <div className="user-info">
-        <button id="logoutbutton" className="logout-button" onClick={handleLogout}>התנתקות</button>
-      </div>
-      <Menu />
-      <div className="welcome-window">
-        <h1 id="welcomeMessage">Welcome, {username}</h1>
-        <p id="description">Here is a brief description of the dashboard and what you can do here. Feel free to explore the features and functionalities tailored for you.</p>
-      </div>
+  if (!authorized) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div>
+      <LogoutButton />
+      <Menu userRole={userRole} />
+      <WelcomeContainer userName={userName} />
     </div>
   );
 }
