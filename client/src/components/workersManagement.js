44462953d1ac274918b7bf6007da868cbc83c3f5
EladAM52 <eladamir46@gmail.com>
2024-02-28 00:16:14 +0200

    scrolldown
    addig scroll down by using overflow auto to adjust the user table.

--- client/src/components/workersManagement.js
+++ client/src/components/workersManagement.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from "react";
 import "../css/workersManagement.css";
 
@@ -9,52 +8,58 @@ const UsersTable = () => {
   useEffect(() => {
     setIsLoading(true);
     fetch("/getusers")
-    .then((response) => response.json())
-    .then((data) => {
-      setUsers(data);
-      setIsLoading(false); 
-    })
-    .catch((error) => {
-      console.error("Error fetching users:", error);
-      setIsLoading(false); 
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        setUsers(data);
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        console.error("Error fetching users:", error);
+        setIsLoading(false);
+      });
   }, []);
 
   if (isLoading) {
-    return <div className="loading-container"><div className="spinner"></div></div>;
-}
+    return (
+      <div className="loading-container">
+        <div className="spinner"></div>
+      </div>
+    );
+  }
 
   return (
-    <div className="users-table-container">
+    <>
       <div className="add-user-container">
-        <button className="add-user-button">
-          הוסף משתמש חדש
-        </button>
+        <button className="add-user-button">הוסף משתמש חדש</button>
       </div>
-      <table className="users-table" dir="rtl">
-        <thead>
-          <tr>
-            <th>שם משתמש</th>
-            <th>אימייל</th>
-            <th>תפקיד</th>
-            <th>פעולות</th>
-          </tr>
-        </thead>
-        <tbody>
-          {users.map((user) => (
-            <tr key={user._id}>
-              <td>{user.username}</td>
-              <td>{user.email}</td>
-              <td>{user.role}</td>
-              <td>
-                <button className="edit-button">ערוך</button>
-                <button className="delete-button">מחק</button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
+      <div className="tablescroll">
+        <div className="users-table-container">
+          <table className="users-table" dir="rtl">
+            <thead>
+              <tr>
+                <th>שם משתמש</th>
+                <th>אימייל</th>
+                <th>תפקיד</th>
+                <th>פעולות</th>
+              </tr>
+            </thead>
+            <tbody>
+              {users.map((user) => (
+                <tr key={user._id}>
+                  <td>{user.username}</td>
+                  <td>{user.email}</td>
+                  <td>{user.role}</td>
+                  <td>
+                    <button className="edit-button">ערוך</button>
+                    <button className="delete-button">מחק</button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </>
   );
 };
 
